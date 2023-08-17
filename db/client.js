2d3cd0fb9b18abca9e2d4c554c6ba441a557ce26
syongk69 <113334586+syongk69@users.noreply.github.com>
2023-08-17 14:52:53 -0700

    secure the DATABASE_URL using .env file

--- db/client.js
+++ db/client.js
@@ -1,8 +1,23 @@
-const { Client } = require("pg");
+// const { Client } = require("pg");
 
-const connectionString =
-  process.env.DATABASE_URL ||
-  "postgres://ncxpmpea:uCegD0FKbCjwI0OGGwbPkydqQTgIixSp@bubble.db.elephantsql.com/ncxpmpea";
+// const connectionString =
+//   process.env.DATABASE_URL ||
+//   "postgres://ncxpmpea:uCegD0FKbCjwI0OGGwbPkydqQTgIixSp@bubble.db.elephantsql.com/ncxpmpea";
+
+// const client = new Client({
+//   connectionString,
+//   ssl:
+//     process.env.NODE_ENV === "production"
+//       ? { rejectUnauthorized: false }
+//       : undefined,
+// });
+
+// module.exports = client;
+
+const { Client } = require("pg");
+require("dotenv").config();
+
+const connectionString = process.env.DATABASE_URL;
 
 const client = new Client({
   connectionString,
